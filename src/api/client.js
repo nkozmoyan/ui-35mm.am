f2704eb05136784d970ef46d52497fded7fae644
Narek <33621622+nkozmoyan@users.noreply.github.com>
2025-07-30 17:35:07 -0700

    fix: improve validation and error handling

--- src/api/client.js
+++ src/api/client.js
@@ -6,6 +6,23 @@ const api = axios.create({
   baseURL: API_URL,
 });
 
+export const getErrorMessage = (err) => {
+  if (err.response) {
+    return err.response.data?.message || 'An error occurred.';
+  } else if (err.request) {
+    return 'Unable to reach the server. Please try again later.';
+  }
+  return err.message;
+};
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    err.message = getErrorMessage(err);
+    return Promise.reject(err);
+  }
+);
+
 export const setAuthToken = (token) => {
   if (token) {
     api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
--- src/components/ChangePassword/ChangePassword.js
+++ src/components/ChangePassword/ChangePassword.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { Form } from '../../Styled';
 import { inputState } from '../../forms/Input';
 import { useSearchParams } from 'react-router-dom';
-import api from '../../api/client';
+import api, { getErrorMessage } from '../../api/client';
 
 const Container = styled.div`
   margin: 5em auto;
@@ -27,7 +27,7 @@ export const ChangePassword = () => {
       await api.post('/password/reset', { token, password: password1.value });
       setMessage('Password updated');
     } catch (err) {
-      setMessage('Failed to update password');
+      setMessage(getErrorMessage(err));
     }
   };
 
@@ -37,14 +37,28 @@ export const ChangePassword = () => {
       <form onSubmit={handleSubmit}>
         <Form.Group>
           <Form.Label htmlFor="password1">New Password:</Form.Label>
-          <Form.Input id="password1" type="password" onChange={setPassword1} value={password1.value} required />
+          <Form.Input
+            id="password1"
+            type="password"
+            onChange={setPassword1}
+            value={password1.value}
+            required
+          />
         </Form.Group>
         <Form.Group>
           <Form.Label htmlFor="password2">Confirm Password:</Form.Label>
-          <Form.Input id="password2" type="password" onChange={setPassword2} value={password2.value} required />
-          {password1.value && password2.value && password1.value !== password2.value && (
-            <Form.FeedBack>Passwords do not match.</Form.FeedBack>
-          )}
+          <Form.Input
+            id="password2"
+            type="password"
+            onChange={setPassword2}
+            value={password2.value}
+            required
+          />
+          {password1.value &&
+            password2.value &&
+            password1.value !== password2.value && (
+              <Form.FeedBack>Passwords do not match.</Form.FeedBack>
+            )}
         </Form.Group>
         {message && <div>{message}</div>}
         <Form.Button type="submit">Change Password</Form.Button>
